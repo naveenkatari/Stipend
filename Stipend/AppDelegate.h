3491c4e8c09fc34f8a96e171f6f7f11eecaf422c
Naveen Katari <naveen.katari@sourcebits.com>
2015-11-25 12:13:42 +0530

    Initial Commit

---
+++ Stipend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stipend
+//
+//  Created by Naveen Katari on 25/11/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
